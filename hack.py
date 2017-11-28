59845be5bc0a5fe4e90586f4cbb3de7581cdb3cc
drmayx <drmayx@github.com>
2017-11-28 13:11:35 +0100

    commit #1

--- hack.py
+++ hack.py
@@ -1,10 +1,15 @@
 from os import system, name
 
-for i in range(100):
-    with open('plik.txt', 'a') as file:
-        file.write('X{}'.format(i))
-    system('git add plik.txt')
-    system('git commit -m "commie commit {}"'.format(i))
-    system('git push')
-    system('drmayx')
-    system('kotpies123')
+def main():
+    i=1
+    while True:
+        with open('plik.txt', 'a') as file:
+            file.write('X{}'.format(i))
+        system('git add .')
+        system('git commit -m "commit #{}"'.format(i))
+        if i%100:
+            system('git push')
+
+        i += 1
+    
+main()
